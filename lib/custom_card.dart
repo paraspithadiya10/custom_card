8d17a27093f1481e1b462518ad217d90850dcf49
Paras Pithadiya <paraspithadiya10@gmail.com>
2025-06-26 15:34:59 +0530

    Update LICENSE, pubspec.yaml, README.md, and custom_card implementation
    - Added MIT License to LICENSE file.
    - Updated package description in pubspec.yaml to reflect the purpose of customizing card design for ListTile.
    - Revised README.md to include a more specific description of the package.
    - Implemented CustomCard class in custom_card.dart with a method to create a styled card using ListTile.
    - Removed outdated Calculator class and its tests in custom_card_test.dart, focusing on the new CustomCard functionality.

--- lib/custom_card.dart
+++ lib/custom_card.dart
@@ -1,5 +1,21 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class CustomCard {
+  show(
+    IconData leadingIcon,
+    String title,
+    String subTitle,
+    IconData trailingIcon,
+  ) {
+    return Card.outlined(
+      elevation: 4,
+      child: ListTile(
+        subtitleTextStyle: TextStyle(fontStyle: FontStyle.italic),
+        leading: Icon(leadingIcon, color: Colors.black),
+        title: Text(title),
+        subtitle: Text(subTitle),
+        trailing: Icon(trailingIcon, color: Colors.black),
+      ),
+    );
+  }
 }
--- test/custom_card_test.dart
+++ test/custom_card_test.dart
@@ -4,9 +4,6 @@ import 'package:custom_card/custom_card.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+    CustomCard();
   });
 }
